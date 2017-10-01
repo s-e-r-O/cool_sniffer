726cb3502120e951046be5199807cea3f0de10af
Sergio Oporto Tejerina <ser.op.te@gmail.com>
2017-10-01 12:42:13 +0000

    Added base program

---
+++ Sniffer/header_reader.c
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pcap/pcap.h>
+
+#include "header_reader.h"
+
+
+int ether_reader(const u_char *bytes)
+{
+
+	struct ether_header *headerEthernet = (struct ether_header *) bytes;
+  
+  	printf("MAC Destination:	%s\n", ether_ntoa((struct ether_addr *) headerEthernet->ether_dhost));
+  	printf("MAC Source:	%s\n", ether_ntoa((struct ether_addr *) headerEthernet->ether_shost));
+
+  	printf("Ether Type:	0x%04x\n", ntohs(headerEthernet->ether_type));  
+
+
+  	switch(ntohs(headerEthernet->ether_type))
+  	{
+    	case ETHERTYPE_IP:
+    			printf("\nETHERTYPE:	IP\n");
+				ip_reader(bytes + sizeof(*headerEthernet));
+    	break;
+    	case ETHERTYPE_ARP:
+    			printf("\nETHERTYPE:	ARP\n");
+				arp_reader(bytes + sizeof(*headerEthernet));
+      	break;
+    	case ETHERTYPE_IPV6:
+    			printf("\nETHERTYPE:	IPv6\n");
+				ipv6_reader(bytes + sizeof(*headerEthernet));
+      	break;
+    }
+}
+
+
+int ip_reader(const u_char *bytes)
+{
+	struct ip *headerIP = (struct ip *) bytes;
+  
+  	printf("Version: %u\n", headerIP->ip_v);
+  	printf("Internet Header Length:	%u\n", headerIP->ip_hl);
+  	printf("Type Of Service: %u\n", headerIP->ip_tos);
+  	printf("Size Of Datagram Or Total Length: %u\n", headerIP->ip_len);
+  	printf("Identification Tag: %u\n", headerIP->ip_id);
+  	printf("Flags: %u\n", headerIP->ip_tos);
+  	printf("Fragment Offset: %u\n", headerIP->ip_off);
+  	printf("Time To Live TTL: %u\n", headerIP->ip_ttl);
+  	printf("Protocol: %u\n", headerIP->ip_p);
+  	printf("Header Checksum: %u\n", headerIP->ip_sum);
+  	printf("Source Address:		%s\n", inet_ntoa(headerIP->ip_src));
+  	printf("Destination Address:	%s\n", inet_ntoa(headerIP->ip_dst));
+
+  	switch(headerIP->ip_p)
+	{
+		case IPPROTO_TCP:
+	    		tcp_reader(bytes + headerIP -> ip_hl * 4);
+	    break;
+	    case IPPROTO_UDP:
+	    		udp_reader(bytes + headerIP -> ip_hl * 4);
+	    break;
+	}
+
+}
+
+int arp_reader(const u_char *bytes)
+{
+
+
+}
+int ipv6_reader(const u_char *bytes)
+{
+	struct ip6_hdr *headerIPv6 = (struct ip6_hdr *) bytes;
+
+	//printf("Source: %s\n", inet_ntop(AF_INET6, *(headerIPv6->ip6_src), NULL, INET6_ADDRSTRLEN));
+  	//printf("Destination: %s\n", inet_ntop(AF_INET6, *(headerIPv6->ip6_dst), NULL, INET6_ADDRSTRLEN));
+}
+
+int tcp_reader(const u_char *bytes)
+{
+
+}
+int udp_reader(const u_char *bytes)
+{
+
+}
+
+int http_reader(const u_char *bytes)
+{
+
+}
---
+++ Sniffer/header_reader.h
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pcap/pcap.h>
+#include <net/ethernet.h>
+#include <netinet/in.h>
+#include <netinet/ether.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <arpa/inet.h>
+
+
+/* DATA LINK LAYER */
+int ether_reader(const u_char *bytes);
+
+/* NETWORK LAYER */
+int ip_reader(const u_char *bytes);
+int arp_reader(const u_char *bytes);
+int ipv6_reader(const u_char *bytes);
+
+/* PROTOCOL LAYER */
+int tcp_reader(const u_char *bytes);
+int udp_reader(const u_char *bytes);
+
+/* APP LAYER */
+int http_reader(const u_char *bytes);
+
+
