7eb28733b26ec0573676c9ae32dc9c80b5fd1ba5
Sergio Oporto Tejerina <ser.op.te@gmail.com>
2017-10-01 14:23:21 +0000

    Fixed UDP Reader

--- source/udp_reader.c
+++ source/udp_reader.c
@@ -6,12 +6,12 @@
 
 int udp_reader(const u_char *bytes)
 {
-  sruct udphdr *headerUDP = (struct udphdr *) bytes;
+  struct udphdr *headerUDP = (struct udphdr *) bytes;
   
   printf("Source Port: %u\n", headerUDP->uh_sport);
   printf("Destination Port: %u\n", headerUDP->uh_dport);
   printf("Length: %u\n", headerUDP->uh_ulen);
-  printf("Checksum: %u\n", headerTCP->uh_sum);
+  printf("Checksum: %u\n", headerUDP->uh_sum);
 
   // SEND TO AN APLICATION LAYER FUNCTION THE NEXT PARAMETER 
   // (bytes + headerUDP -> uh_ulen);
