088786b60347f706b85661e0ac0ca0807cfb683a
Eli-C <elicg2012@hotmail.com>
2017-10-01 11:50:11 -0400

    Modificaion de ether header y de ip header

---
+++ source/ether_reader.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pcap/pcap.h>
+
+#include "header_reader.h"
+
+
+int ether_reader(const u_char *bytes)
+{
+	struct ether_header *headerEthernet = (struct ether_header *) bytes;
+  
+    printf("MAC Destination:  %s\n", ether_ntoa((struct ether_addr *) headerEthernet->ether_dhost));
+    printf("MAC Source: %s\n", ether_ntoa((struct ether_addr *) headerEthernet->ether_shost));
+
+    printf("Ether Type: 0x%04x\n", ntohs(headerEthernet->ether_type));  
+
+
+    switch(ntohs(headerEthernet->ether_type))
+    {
+      case ETHERTYPE_IP:
+          printf("\nETHERTYPE:  IP\n");
+        ip_reader(bytes + sizeof(*headerEthernet));
+      break;
+      case ETHERTYPE_ARP:
+          printf("\nETHERTYPE:  ARP\n");
+        arp_reader(bytes + sizeof(*headerEthernet));
+        break;
+      case ETHERTYPE_IPV6:
+          printf("\nETHERTYPE:  IPv6\n");
+        ipv6_reader(bytes + sizeof(*headerEthernet));
+        break;
+    }
+}
---
+++ source/ip_reader.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pcap/pcap.h>
+
+#include "header_reader.h"
+
+int ip_reader(const u_char *bytes)
+{
+	 struct ip *headerIP = (struct ip *) bytes;
+  
+    printf("Version: %u\n", headerIP->ip_v);
+    printf("Internet Header Length: %u\n", headerIP->ip_hl);
+    printf("Type Of Service: %u\n", headerIP->ip_tos);
+    printf("Size Of Datagram Or Total Length: %u\n", headerIP->ip_len);
+    printf("Identification Tag: %u\n", headerIP->ip_id);
+    /*printf("Flag1: %x\n", headerIP->IP_RF);   // reserved bit
+    printf("Flag2: %x\n", headerIP->IP_DF);   // don't fragment
+    printf("Flag3: %x\n", headerIP->IP_MF);   // more fragments
+    printf("Flag4: %x\n", headerIP->IP_OFFMASK);  */    
+    printf("Fragment Offset: %u\n", headerIP->ip_off);
+    printf("Time To Live TTL: %u\n", headerIP->ip_ttl);
+    printf("Protocol: %u\n", headerIP->ip_p);
+    printf("Header Checksum: %u\n", headerIP->ip_sum);
+    printf("Source Address:   %s\n", inet_ntoa(headerIP->ip_src));
+    printf("Destination Address:  %s\n", inet_ntoa(headerIP->ip_dst));
+
+    switch(headerIP->ip_p)
+    {
+      case IPPROTO_TCP:
+          printf("\nPROTOCOL Inside Of The IP Is:  TCP\n");
+            tcp_reader(bytes + headerIP -> ip_hl * 4);
+        break;
+        case IPPROTO_UDP:
+            printf("\nPROTOCOL Inside Of The IP Is:  UDP\n");
+            udp_reader(bytes + headerIP -> ip_hl * 4);
+        break;
+    }
+
+}
