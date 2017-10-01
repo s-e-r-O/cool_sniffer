7eeffd4df3fe4cda68335cc70992db2e3612e64a
Eli-C <elicg2012@hotmail.com>
2017-10-01 19:51:20 -0400

    Modificacion de ARP

--- source/arp_reader.c
+++ source/arp_reader.c
@@ -6,6 +6,15 @@
 
 int arp_reader(const u_char *bytes, bpf_u_int32 dataLength)
 {
+	printf("\n------------------ ARP ------------------\n\n");
 
+
+  	struct arphdr *headerARP = (struct arphdr *) bytes;
+
+	printf("Format Of Hardware Address: %u\n", headerARP->ar_hdr);
+	printf("Format Of Protocol Address: %u\n", headerARP->ar_pro);
+	printf("Length Of Hardware Address: %c\n", headerARP->ar_hln);
+	printf("Length Of Protocol Address: %c\n", headerARP->pln);
+	printf("ARP opcode: %u\n", headerARP->ar_op);
 
 }
