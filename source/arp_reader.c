079e93d40151e24de88e2e2d0e9b233991717795
Eli-C <elicg2012@hotmail.com>
2017-10-01 20:13:59 -0400

    m

--- source/arp_reader.c
+++ source/arp_reader.c
@@ -12,10 +12,10 @@ int arp_reader(const u_char *bytes, bpf_u_int32 dataLength)
 
   	struct arphdr *headerARP = (struct arphdr *) bytes;
 
-	printf("Format Of Hardware Address: %u\n", headerARP->ar_hdr);
+	printf("Format Of Hardware Address: %u\n", headerARP->ar_hrd);
 	printf("Format Of Protocol Address: %u\n", headerARP->ar_pro);
 	printf("Length Of Hardware Address: %c\n", headerARP->ar_hln);
-	printf("Length Of Protocol Address: %c\n", headerARP->pln);
+	printf("Length Of Protocol Address: %c\n", headerARP->ar_pln);
 	printf("ARP opcode: %u\n", headerARP->ar_op);
 
 }
